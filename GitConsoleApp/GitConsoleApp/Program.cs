c78eb8fc74248911ab24283cbe8e7fedf366ce6a
Sandeep Singh <er.sandeep860@gmail.com>
2017-10-12 08:36:04 +0200

    New commit

---
+++ GitConsoleApp/GitConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is repo connection change 1 2 3");
+            Console.WriteLine("Jenkins Build");
+            Console.ReadLine();
+        }
+    }
+}
